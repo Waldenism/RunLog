8412b56e4840a4b9edc924dc3211462b6cf8eed5
momojunzi <mark.holthaus@gmail.com>
2017-11-04 13:40:05 -0700

    login and register ajax merged with latest master

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -30,28 +30,22 @@ module.exports = function(app) {
 
   app.post('/login', function(req, res){
     console.log(req.body);
-
-    // tests to test the functionality of the login
-    for(var i=0; i<testAuth.length; i++){
-      //test if user name and password are in test var
-      if(req.body.user_name === testAuth[i].user_name && req.body.password_hash === testAuth[i].password_hash){
-        var userFound = {
-          status: 200,
-          message: testAuth[i].user_id
-        }
-        //respond with a user var if found
-        res.json(userFound);
+    let email = req.body.email;
+    db.User.findOne({
+      where:{
+        user_email: email
       }
-      //if not found
-      if(i===testAuth.length -1 && req.body.user_name !== testAuth[i].user_name){
-        var userNotFound = {
-          status: 404,
-          message: "User name or password not found. Please try again or register."
-        };
-        //respond with a user not found message and redirect to try again or login
-        res.json(userNotFound);
+    }).then(function(data) {
+      console.log(data);
+      if(data===null) {
+        //send 404 back to front end to direct user to register
+        res.json({status: 404, message: "user not found try again or register"});
       }
-    }
+      else{
+        // send 200 to user to redirect to run page.
+        res.json({status: 200});
+      }
+    });
   });
 
   //test posted data from register form.
@@ -61,39 +55,5 @@ module.exports = function(app) {
     res.json({status:200});
     // TODO: store hashed (+salted) password in db and redirect to login
     res.redirect('/login');
-
-
-    //test to see if user is added to tesy var. in the real world this
-    //would post to db. testid would be set by db etc.
-    // var testid = testAuth.length;
-    // newUser.user_id = testid;
-    // testAuth.push(newUser);
-    // console.log(newUser);
-    // console.log(testAuth);
-    // var secondTest = testAuth.length+1;
-    // if(secondTest > testid){
-    //   res.json({status:200});
-    // }else {res.json({status: 500});}
   });
 };
-
-
-
-//test var
-var testAuth = [
-  {
-    user_id: 1,
-    name: 'mark',
-    user_name: 'mark',
-    email: 'mark@mark.mark',
-    password_hash: '1111'
-  },
-  {
-    user_id: 2,
-    name: 'chris',
-    user_name: 'chris',
-    email: 'chris@chris.chris',
-    password_hash: '2222'
-  }
-];
-//end test var
