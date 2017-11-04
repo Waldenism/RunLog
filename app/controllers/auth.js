d5b463accc3111ab81a880eb1018a40384cec6bb
mmenschig <marian.menschig@googlemail.com>
2017-11-04 11:25:13 -0700

    update auth controller and fix login path

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,18 +1,38 @@
 let db = require('../db/models');
+let bcrypt = require('bcrypt');
 
 module.exports = function(app) {
 
   app.get('/login', function(req, res) {
-
-    db.User.findAll().then(function(data) {
-      console.log(data);
-    })
-
     res.render('auth/login');
+  })
+
+  app.post('/login', function(req, res) {
+    let email = req.body.email
+
+    // TODO: hash password and compare with 
+    // stored hash via email/username retrieval
+
+    db.User.findOne({
+      where: {
+        email: email
+      }
+    })
+    .then(function(data) {
+      // if hashed pw = stored hash
+      // create valid session
+      // and redirect to athlete view
+      // else we reload login with express-flash
+    })
   })
 
   app.get('/register', function(req, res) {
     res.render('auth/register');
+  })
+
+  app.post('/register', function(req, res) {
+    // TODO: store hashed (+salted) password in db and redirect to login
+    res.redirect('auth/login')
   })
 
 }
