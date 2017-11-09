1ba63742803289a9fefd07e9a4a3194467be9283
momojunzi <mark.holthaus@gmail.com>
2017-11-09 06:20:08 -0800

    conflicts resolved

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -2,37 +2,6 @@ var db = require('../db/models');
 
 module.exports = function(app) {
 
-<<<<<<< 91e7487fc0ddcecb3c28dcb1bf7b4bd83964baf7
-  app.get('/', function(req,res) {
-
-    // TODO: get current user from auth
-
-    db.Run.findAll({
-      where: {
-        user_user_id: 1
-      }
-    })
-    .then(function(runs) {
-
-      var events = [];
-      for (var i = 0; i < runs.length; i++) {
-        var entry = {}
-
-        entry.title = String(runs[i].dataValues.run_distance) + 'km';
-        entry.start = runs[i].dataValues.run_date;
-
-        events.push(entry);
-      }
-      console.log(events);
-      // passes the events data to the calendarView
-      res.render('calendarView.hbs', {events: events});
-    })
-  })
-
-  app.get('/calendar', function(req, res) {
-    res.render('calendarView.hbs');
-  });
-=======
 let user;
 //handle get request to index
 app.post('/', function(req, res){
@@ -44,42 +13,30 @@ app.get('/', function(req,res){
   if(!user){
     res.redirect('/login');
   }else{
-    res.render('calendarView');
-  }
-}),
-  // app.get('/', function(req,res) {
-  //
-    // console.log(req.body);
-    // //if there is no user data sent with the GET
-    // if(!req.body.user){
-    //   res.redirect('/login');
-    // }
-    // //if there is a user sent with the GET
-    // else{
-    //   db.Run.findAll({
-    //   where: {
-    //     user_user_id: 1
-    //   }
-    // })
-    // .then(function(runs) {
-    //
-    //   // let events = [];
-    //   // for (let i = 0; i < runs.length; i++) {
-    //   //   let entry = {}
-    //   //
-    //   //   entry.title = String(runs[i].dataValues.run_distance) + 'km';
-    //   //   entry.start = runs[i].dataValues.run_date;
-    //   //
-    //   //   events.push(entry);
-    //   // }
-    //   // console.log(events);
-    //   // // passes the events data to the calendarView
-    //   // res.render('calendarView.hbs', {events: events});
-    // })
-    // }
 
-//  })
->>>>>>> some work to fix
+    db.Run.findAll({
+      where: {
+        user_user_id: 1
+      }
+    })
+    .then(function(runs) {
+
+      let events = [];
+      for (let i = 0; i < runs.length; i++) {
+        let entry = {}
+
+        entry.title = String(runs[i].dataValues.run_distance) + 'km';
+        entry.start = runs[i].dataValues.run_date;
+
+        events.push(entry);
+      }
+      console.log(events);
+      // passes the events data to the calendarView
+      res.render('calendarView.hbs', {events: events});
+    })
+    }
+
+ })
 
   app.get('/logrun', function(req, res) {
     res.render('log.hbs');
--- app/public/assets/js/script.js
+++ app/public/assets/js/script.js
@@ -30,12 +30,7 @@ $(function(){
     }
     //login ajax
     $.post('/login', loginInfo, function(data){
-<<<<<<< 91e7487fc0ddcecb3c28dcb1bf7b4bd83964baf7
-=======
-      appData.userId = data.user;
       sessionStorage.setItem('user', data.user);
-      $.post('/',{user: sessionStorage.user});
->>>>>>> some work to fix
       window.location.href = data.redirect;
     })
   });
@@ -45,18 +40,10 @@ $(function(){
     console.log('clicked');
     event.preventDefault();
     var registerInfo = {
-<<<<<<< 91e7487fc0ddcecb3c28dcb1bf7b4bd83964baf7
       user_alias:$('#registerUserName').val().trim(),
       user_name:$('#registerUserName').val().trim(),
       user_email:$('#registerEmail').val().trim(),
       password_hash:$('#registerPassword').val().trim(),
-=======
-      user_name: $('#registerName').val().trim(),
-      user_alias:$('#registerUserName').val().trim(),
-      user_email:$('#registerEmail').val().trim(),
-      password_hash:$('#registerPassword').val(),
-
->>>>>>> some work to fix
     };
     $.post('/register', registerInfo, function(data){
       console.log('login data poasted');
@@ -64,42 +51,6 @@ $(function(){
     });
   });
 
-<<<<<<< 91e7487fc0ddcecb3c28dcb1bf7b4bd83964baf7
-=======
-  $('#logRunBtn').on('click', function(){
-    console.log(appData.userId);
-    window.location.href = '/logrun';
-  })
-
-  //listen for an event on the feeling button
-  $('.feeling-button').on('click', function(){
-   appData.feeling = $(this).data('feeling');
- });
-
-   //listen for event on lig run button
-   $('#logRunButton').on('click', function(){
-      if(feeling === undefined){
-        alert('please choose how you felt for the run');
-      }
-      else{
-        runInfo = {
-          distance: $('#logDistance').val().trim(),
-          time:$('#logTime').val().trim(),
-          feeling: feeling,
-          message: $('#injury').val().trim()
-
-
-        };
-      }
-      $.post('/logrun', runInfo, function(data){
-        if(data.status === 200) {
-          console.log('run added');
-          window.location.href = '/calendar';
-        }
-      });
-    });
-
->>>>>>> some work to fix
  //listen for event on lig run button
  $('#submitRun').on('click', function(){
     if(appData.feeling === undefined){
